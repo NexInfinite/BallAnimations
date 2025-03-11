a4313f54657acf5558a83c3bf012cf926e0d8185
Julian Jones <julianjones663@gmail.com>
2025-03-11 22:04:37 +0000

    quick tutorial done

---
+++ src/main.rs
@@ -0,0 +1,50 @@
+use bevy::prelude::*;
+
+fn main() {
+    App::new()
+        .add_plugins(DefaultPlugins)
+        .add_plugins(HelloPlugin)
+        .run();
+}
+
+pub struct HelloPlugin;
+
+impl Plugin for HelloPlugin {
+    fn build(&self, app: &mut App) {
+        app.insert_resource(GreetTimer(Timer::from_seconds(2.0, TimerMode::Repeating)));
+        app.add_systems(Startup, add_people);
+        app.add_systems(Update, (update_people, greet_people).chain());
+    }
+}
+
+fn add_people(mut commands: Commands) {
+    commands.spawn((Person, Name("Julian Jones".to_string())));
+    commands.spawn((Person, Name("Mac Jones".to_string())));
+    commands.spawn((Person, Name("Harry Jones".to_string())));
+}
+
+fn greet_people(time: Res<Time>, mut timer: ResMut<GreetTimer>, query: Query<&Name, With<Person>>) {
+    if timer.0.tick(time.delta()).just_finished() {
+        for name in &query {
+            println!("hello {}!", name.0);
+        }
+    }
+}
+
+fn update_people(mut query: Query<&mut Name, With<Person>>) {
+    for mut name in &mut query {
+        if name.0 == "Julian Jones" {
+            name.0 = "Boolean Bones".to_string();
+            break;
+        }
+    }
+}
+
+#[derive(Component)]
+struct Person;
+
+#[derive(Component)]
+struct Name(String);
+
+#[derive(Resource)]
+struct GreetTimer(Timer);
