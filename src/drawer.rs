4ec232bbe9c30b2343eaf3aa1f9a85adeed8cd1c
Julian Jones <julianjones663@gmail.com>
2025-03-15 18:07:50 +0000

    allowed new balls to be added

--- src/drawer.rs
+++ src/drawer.rs
@@ -11,14 +11,54 @@ pub struct Velocity {
 }
 
 #[derive(Default)]
-struct BallsConfig {
+struct GlobalBallsConfig {
     balls_move: bool,
+}
+
+#[derive(Default)]
+struct BallStyle {
+    size: f32,
+    color: Color,
+}
+
+#[allow(dead_code)]
+impl BallStyle {
+    const DEFAULT_SIZE: f32 = 15.0;
+    const DEFAULT_COLOR: Color = Color::srgb(0.1, 0.1, 0.1);
+
+    fn new(size: f32, color: Color) -> BallStyle {
+        BallStyle {
+            size: size,
+            color: color,
+        }
+    }
+
+    fn new_size_only(size: f32) -> BallStyle {
+        BallStyle {
+            size: size,
+            color: BallStyle::DEFAULT_COLOR,
+        }
+    }
+
+    fn new_color_only(color: Color) -> BallStyle {
+        BallStyle {
+            size: BallStyle::DEFAULT_SIZE,
+            color: color,
+        }
+    }
+
+    fn default() -> BallStyle {
+        BallStyle {
+            size: BallStyle::DEFAULT_SIZE,
+            color: BallStyle::DEFAULT_COLOR,
+        }
+    }
 }
 
 pub struct DrawBalls;
 impl Plugin for DrawBalls {
     fn build(&self, app: &mut App) {
-        let mut config = BallsConfig { balls_move: true };
+        let mut config = GlobalBallsConfig { balls_move: true };
 
         app.add_systems(Startup, (draw_balls, draw_text));
         app.add_systems(Update, handle_collision);
@@ -41,27 +81,29 @@ fn draw_balls(
 ) {
     commands.spawn(Camera2d);
 
-    let color = Color::srgb(0.1, 0.1, 0.1);
-    let ball_size = 15.0;
-    let balls = [(
-        meshes.add(Circle::new(ball_size)),
+    let mut balls = Vec::<(Velocity, Transform, BallStyle)>::new();
+    balls.push((
+        Velocity { x: 10.0, y: -10.0 },
         Transform::from_xyz(-500.0, 0.0, 0.0),
-    )];
+        BallStyle::default(),
+    ));
+    balls.push((
+        Velocity { x: 5.0, y: -10.0 },
+        Transform::from_xyz(0.0, 0.0, 0.0),
+        BallStyle::new_color_only(Color::srgb(0.4, 0.1, 0.1)),
+    ));
 
-    for (_, ball) in balls.into_iter().enumerate() {
-        commands.spawn((
-            Ball,
-            Mesh2d(ball.0),
-            MeshMaterial2d(materials.add(color)),
-            ball.1,
-            Velocity { x: 10.0, y: -10.0 },
-        ));
+    for ball in balls {
+        let mesh = meshes.add(Circle::new(ball.2.size));
+        let material = materials.add(ball.2.color);
+
+        commands.spawn((Ball, Mesh2d(mesh), MeshMaterial2d(material), ball.1, ball.0));
     }
 }
 
 fn move_balls(
     mut query: Query<(&mut Transform, &Velocity), With<Ball>>,
-    config: &mut BallsConfig,
+    config: &mut GlobalBallsConfig,
     window: Query<&Window>,
 ) {
     let half_height = window.single().resolution.height() / 2.0;
@@ -111,7 +153,7 @@ fn handle_collision(
 
 fn draw_text(mut commands: Commands) {
     commands.spawn((
-        Text::new("What da sigma: Press space to debug"),
+        Text::new("Press space to debug"),
         Node {
             position_type: PositionType::Absolute,
             top: Val::Px(12.0),
@@ -121,7 +163,7 @@ fn draw_text(mut commands: Commands) {
     ));
 }
 
-fn on_window_resize(mut resize_reader: EventReader<WindowResized>, config: &mut BallsConfig) {
+fn on_window_resize(mut resize_reader: EventReader<WindowResized>, config: &mut GlobalBallsConfig) {
     if resize_reader.read().len() == 0 {
         config.balls_move = true;
     }
